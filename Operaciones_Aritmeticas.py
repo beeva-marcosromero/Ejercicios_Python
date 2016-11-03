688c885431d8037ac3b57baa1b581839d5df7485
Marcos Romero <marcos.romero@beeva.com>
2016-11-03 13:14:13 +0100

    Ejercicio con operaciones aritmeticas

---
+++ Operaciones_Aritmeticas.py
@@ -0,0 +1,28 @@
+a = 5
+b = 10.4
+c = 32
+d = 87.2
+
+#Suma
+print "Suma = ",
+print a+b+c+d
+
+#Resta
+print "Resta = ",
+print d-c
+
+#Multiplicacion
+print "Multiplicacion = ",
+print a*c
+
+#Exponente
+print "Exponente = ",
+print a**2
+
+#Division
+print "Division = ",
+print c/a
+
+#Modulo
+print "Modulo = ",
+print d%b
